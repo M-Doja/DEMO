ba3a0e631c77fc8749adc9f7ec6acedee01af0a7
M-Doja <micdoja@yahoo.com>
2016-02-26 23:56:10 -0800

    blog finished

--- public/JS/app.js
+++ public/JS/app.js
@@ -19,30 +19,21 @@ $(document).ready(function(){
     // $('#all_elements .element').sort(sortDescending).appendTo('#all_elements');
 
 var settings = $.extend({
-
 // menu selector
 'menu': ('#menu'),
-
 // trigger selector
 'push': ('.push'),
-
 // The side of the navigation menu (either 'right' or 'left')
 'side': 'left',
-
 // the width of your menu
 'menuWidth': '15.625em',
-
 // animation speed
 'speed': '300',
-
 'state': 'closed',
-
 // CSS class added to the menu button when it is open
 'activeBtn': 'active',
-
 // allow users to toggle the menu closed with any click
 'easyClose': false
-
 }, options);
 
 
@@ -65,131 +56,96 @@ win.scroll(function() {
 
 // BLOG PAGE-----------------------------------------------------------
 
-
-//  Creates an empty Array
-var blogs = [];
+// TOGGLE FORM TO ADD NEW BLOG
 function showForm() {
     $(".form").toggle();
-    $("#ctrlBtn").hide();
+    // $("#ctrlBtn").hide();
 }
 function closeForm() {
   $(".form").toggle();
-  $("#ctrlBtn").show();
+  // $("#ctrlBtn").show();
 }
-$("li").sort(function(a,b){
-    return new Date($(a)) > new Date($(b));
-}).each(function(){
-    $("#blogs").prepend(this);
-})
 
-function BlogPost(title, description, body, pic, author, Date){
+// SHOW WELCOME MSG TO ADMIN OR VISITOR
+$('#visitor').css( 'display', 'inline-block');
+$('#admin').css( 'display', 'none');
+
+// WILL DISPLAY ADMIN CTRLS IF ADMINKEY IS ENTERED
+document.getElementById('AdminBtn').addEventListener('click', function() {
+  var Key = 'admin';
+  var name = $('#AdminName').val();
+  var keyStr = document.getElementById('key').value;
+  var adminkey = keyStr.toLowerCase();
+    if(adminkey === Key) {
+
+      // $('#admin').css( 'display', 'inline-block')
+      $("#msg").html("Welcome Admin " + name);
+      $('#delBtn').css( 'display', 'inline-block');
+      // $('.editBtn').css( 'display', 'inline-block');
+      $('#visitor').css( 'display', 'none');
+    }
+    else {
+      alert('Please enter a valid Admin Key!');
+      $('#visitor').css( 'display', 'inline-block')
+      document.getElementById('key').value = '';
+      $('#AdminName').val('');
+    }
+    console.log(adminkey);
+    document.getElementById('key').value = '';
+    $('#AdminName').val('');
+}, false);
+
+// SORT BLOGS
+// $("li").sort(function(a,b){
+//     return new Date($(a)) > new Date($(b));
+// }).each(function(){
+//     $("#blogs").prepend(this);
+// })
+
+//  Creates an empty Array
+var blogs = [];
+
+// BLOG CONSTRUCTOR FUNCTIION
+function BlogPost(title, description, body, pic, author, date){
   this.title = title;
   this.description = description;
   this.body = body;
   this.pic = pic;
   this.author = author;
-  this.date = new Date;
+  this.date = new Date();
 }
+console.log(BlogPost);
 
-function getBlogs(){
-  var req = new XMLHttpRequest();
-  req.open("GET", "https://pages-blog.firebaseio.com/.json");
-  req.onload = function(){
-  console.log(req);
-  if (200 <= this.status < 400){
-    var res = JSON.parse(this.response);
-    console.log(res);
-    var elemStr = "";
-    blogs.length = 0; // empties the array
-    for (var prop in res){
-      res[prop]._id = prop;
-      blogs.push(res[prop]);
-      console.log(res);
-      elemStr += "<br><li><span id='BlogTitle'>" + res[prop].title + "</span><br>Summary:  "
-      + res[prop].description + " <br>Author: <b>"
-      + res[prop].author + "</b><br>Published: " +  res[prop].date + "<br><br>" +  res[prop].body + "<br><br>"
-      + "<button class='editBtn' style='margin-left:10px; text-align:center;' class='btn btn-warning btn-sm' onclick='startEdit(" + (blogs.length - 1) + ")'>Edit</button>"
-      + "</li><hr>"
-      // console.log(res[prop]);
-      $("#blogMessage").hide();
-      $("#buttonsGoHere").html('');
-    }
-
-    document.getElementById('blogs').innerHTML = elemStr;
-    // console.log(res);
-    }else {
-      console.log(this.response);
-    }
-  }
-  req.send();
-}
-getBlogs();
-
-//  // THIS CODE BLOCK CONTROLS WHAT IS DISPLAYED WHEN ADMIN KEY IS INPUT
-// document.getElementById('AdminBtn').addEventListener('click', function() {
-//   var Key = 'admin';
-//   var keyStr = document.getElementById('key').value;
-//   var adminkey = keyStr.toLowerCase();
-//     if(adminkey === Key) {
-//       $('.editBtn').css( 'display', 'inline-block')
-//       $('#delBtn').css( 'display', 'inline-block')
-//     }
-//     else {
-//       alert('Please enter a valid Admin Key!');
-//       document.getElementById('key').value = '';
-//     }
-//     console.log(adminkey);
-//     document.getElementById('key').value = '';
-// }, false);
-
-
-function startEdit(index){
-  $("#editTitle").val(blogs[index].title);
-  $("#editDescription").val(blogs[index].description);
-  $("#editBody").val(blogs[index].body);
-  $("#editAuthor").val(blogs[index].author);
-  $("#editSubmitButton").html('<button onclick="saveEdit(' + index +')" class="btn btn-primary">Save Changes</button>');
-  $("#myModal").modal('toggle');
-}
-
-function saveEdit(index){
-  var title = $("#editTitle").val();
-  var description = $("#editDescription").val();
-  var body = $("#editBody").val();
-  var author = $("#editAuthor").val();
-  var blog = new BlogPost(title, description, body, author, Date);
-
-  $.ajax({
-    url: "https://pages-blog.firebaseio.com/" + blogs[index]._id + "/.json",
-    type: "PUT",
-    data: JSON.stringify(blog)
-  }).success(function(res){
-     res = this.response
-    getBlogs();
-  })
-  $("#myModal").modal('toggle');
-}
-
-function saveBlogs(){
+// SAVE A BLOG ENTRY TO DATABASE
+// STORE INPUT IN VARIABLES
+function saveBlog(){
   var title = document.getElementById("blogTitle").value;
   var body = document.getElementById("blogBody").value;
   var description = document.getElementById("blogDescription").value;
+  var pic = document.getElementById("blogPhotoURL").value;
   var author = document.getElementById("blogAuthor").value;
+
   // if feild is empty then return out of function, else execute
   if (title === ""  || body === ""  || author === "") {
     return
   }
-  var blog = new BlogPost(title, description, body, author, Date);
+  var blog = new BlogPost(title, description, body, pic, author, Date);
+  console.log(blog);
+
+  // SAVE BLOG TO DATABASE
   var req = new XMLHttpRequest();
-  req.open('POST',"https://pages-blog.firebaseio.com/.json");
+  req.open('POST',"https://pages-blogger.firebaseio.com/.json");
   req.onload = function(){
       $(".form").hide();
       getBlogs();
   }
+  // CLEAR THE FORM OF DATA FOR NEXT ENTRY
   document.getElementById("blogTitle").value = "";
   document.getElementById("blogDescription").value = "";
   document.getElementById("blogBody").value = "";
+  document.getElementById("blogPhotoURL").value = "";
   document.getElementById("blogAuthor").value = "";
+
   req.send(JSON.stringify(blog));
 
     // setting date created
@@ -200,10 +156,77 @@ function saveBlogs(){
       ((''+month).length<2 ? '0' : '') + month + '/' +
       ((''+day).length<2 ? '0' : '') + day;
 
-document.getElementById("blogs").value = "<br><time>" + output + "</time>";
+  document.getElementById("blogs").value = "<br><time>" + output + "</time>";
 }
 
+// DISPLAY ALL THE BLOGS FROM DATABASE ON THE PAGE
+function getBlogs(){
+  var req = new XMLHttpRequest();
+  req.open("GET", "https://pages-blogger.firebaseio.com/.json");
+  req.onload = function(){
+  console.log(req);
+  if (200 <= this.status < 400){
+    var res = JSON.parse(this.response);
+    console.log(res);
+    var elemStr = "";
+    blogs.length = 0; // empties the array
+    for (var prop in res){
+      res[prop]._id = prop;
+      blogs.push(res[prop]);
+      elemStr += "<br><li><span id='BlogTitle'>" + res[prop].title + "</span><br>"
+      + "<img class='img-responsive' src='"+res[prop].pic +"'><br>"
+      + "Summary:  "+ res[prop].description + "<br>"
+      + "Author: <b>" + res[prop].author + "</b><br>"
+      + "Published: " +  res[prop].date + "<br><br>" +  res[prop].body + "<br><br>"
+      + "<button style='margin-bottom:40px;' class='btn btn-warning btn-sm editBtn' onclick='startEdit(" + (blogs.length - 1) + ")'>Edit</button>"
+      + "</li><a href='#'>Top</a><hr class='hr'>"
+      $("#blogMessage").hide();
+      $("#buttonsGoHere").html('');
+    }
+    document.getElementById('blogs').innerHTML = elemStr;
+    // console.log(res);
+    }else {
+      console.log(this.response);
+    }
+  }
+  req.send();
+}
+getBlogs();
 
+// EDIT A SPECFIC BLOG POST
+// GET DATA ON SPECIFIC BLOG TO EDIT
+function startEdit(index){
+  $("#editTitle").val(blogs[index].title);
+  $("#editDescription").val(blogs[index].description);
+  $("#editBody").val(blogs[index].body);
+  $("#editPic").val(blogs[index].pic);
+  $("#editAuthor").val(blogs[index].author);
+  $("#editSubmitButton").html('<button onclick="saveEdit(' + index +')" class="btn btn-primary">Save Changes</button>');
+  $("#myModal").modal('toggle');
+}
+
+// STORES EDIT INPUT IN NEW BLOG
+function saveEdit(index){
+  var title = $("#editTitle").val();
+  var description = $("#editDescription").val();
+  var body = $("#editBody").val();
+  var pic = $("#editPic").val();
+  var author = $("#editAuthor").val();
+  var blog = new BlogPost(title, description, body, pic, author, Date);
+
+    // SENDING EDIT TO DATABASE FOR SPECIFIC BLOG
+  $.ajax({
+    url: "https://pages-blogger.firebaseio.com/" + blogs[index]._id + "/.json",
+    type: "PUT",
+    data: JSON.stringify(blog)
+  }).success(function(res){
+     res = this.response
+    getBlogs();
+  })
+  $("#myModal").modal('toggle');
+}
+
+// DELETE A SPECFIC BLOG OR BLOGS
 function deleteBlogs() {
   var elemStr = "";
   for (var i=0; i<blogs.length; i+=1){
@@ -211,12 +234,15 @@ function deleteBlogs() {
     }
     $("#blogs").html(elemStr);
     $("#buttonsGoHere").html('<button class="btn btn-success" onclick="saveDelete()">Delete Selected</button><button class="btn btn-danger" onclick="cancel()">Cancel</button>');
-
   }
+
+// CANCEL BLOG DELETING ACTION
 function cancel(){
   $("#buttonsGoHere").html('');
   getBlogs();
 }
+
+// TRACK WHAT BLOG TO DELETE
 var delCount,
     boxes;
 function saveDelete(){
@@ -226,10 +252,12 @@ function saveDelete(){
     deleteBlogsFunc(boxes[0].id);
   }
 }
+
+// DELETING SPECFIC BLOG FROM DATA BASE
 //  recursive function
 function deleteBlogsFunc(id){
   $.ajax({
-    url:'https://pages-blog.firebaseio.com/' + id + '/.json', type: 'DELETE'
+    url:'https://pages-blogger.firebaseio.com/' + id + '/.json', type: 'DELETE'
   }).success(function(){
     delCount += 1;
     if(delCount < boxes.length){
